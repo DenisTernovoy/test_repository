a874a44739c9c3eb54caae3b1a178740f67152e7
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-04-01 14:18:44 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+import time
+
+async def one():
+    print('Start 1')
+    await asyncio.sleep(5)
+    print('End 1')
+
+async def two():
+    print('Start 2')
+    await asyncio.sleep(10)
+    print('End 2')
+
+async def three():
+    print('Start 3')
+    await asyncio.sleep(15)
+    print('End 3')
+
+async def main():
+    await asyncio.gather(one(), two(), three())
+
+if __name__ == '__main__':
+    start = time.time()
+    asyncio.run(main())
+    print(time.time() - start)
