6a784b7406cf3f31e584376966d5914e45b07aff
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-04-01 14:24:22 +0300

    Third commit

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import time
 
 async def one():
     print('Start 1')
-    await asyncio.sleep(5)
+    await asyncio.sleep(2)
     print('End 1')
 
 async def two():
     print('Start 2')
-    await asyncio.sleep(10)
+    await asyncio.sleep(5)
     print('End 2')
 
 async def three():
     print('Start 3')
-    await asyncio.sleep(15)
+    await asyncio.sleep(10)
     print('End 3')
 
 async def main():
